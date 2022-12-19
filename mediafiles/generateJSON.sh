da9ad5533e260bc6cd4fcf88e3c61a61f2900a9e
Angel Rivera Sanz <ariverasanz@gmail.com>
2022-12-19 22:04:14 +0100

    json

---
+++ mediafiles/generateJSON.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+echo 'ObjPL='
+
+echo '[{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "where": "box" 
+  }'
+
+for i in {1..100}   # you can also use {0..9}
+do
+  j=$(expr ${i} '*' 3000)
+  echo ',{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons('${j}')).toISOString(), 
+    "where": "box" 
+  }' 
+done
+
+echo '];'
+
+# var_date=$(date)
+
+# x=1
+# while [ $x -le 50 ]
+# do
+#   var_date=$(date -d "${var_date} + 10 seconds") 
+#   echo "${var_date}";../media/Amazon3.jpg
+#   var_date=$(date -d "${var_date} + 10 seconds" ) 
+#   echo "${var_date}";../media/Runners-8153.mp4
+#   var_date=$(date -d "${var_date} + 10 seconds") 
+#   echo "${var_date}";../media/E56DAA3C-E756-4EF8-C06E-E989C47E827E.mp4		
+#   var_date=$(date -d "${var_date} + 10 seconds") 
+#   echo "${var_date}";../media/4.jpg
+#   x=$(( $x + 1 ))
+# done
--- mediafiles/list.js
+++ mediafiles/list.js
@@ -14,55 +14,595 @@ ObjPL=
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(6000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(9000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(12000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(15000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(18000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(21000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(24000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(27000)).toISOString(), 
     "where": "box" 
   }
 ,{ 
     "src": "image3.jpg", 
     "mime": "image/jpeg", 
-    "when":  new Date(new Date().addMilisecons(3000)).toISOString(), 
+    "when":  new Date(new Date().addMilisecons(30000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(33000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(36000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(39000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(42000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(45000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(48000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(51000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(54000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(57000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(60000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(63000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(66000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(69000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(72000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(75000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(78000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(81000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(84000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(87000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(90000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(93000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(96000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(99000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(102000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(105000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(108000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(111000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(114000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(117000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(120000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(123000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(126000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(129000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(132000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(135000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(138000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(141000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(144000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(147000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(150000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(153000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(156000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(159000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(162000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(165000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(168000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(171000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(174000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(177000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(180000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(183000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(186000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(189000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(192000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(195000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(198000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(201000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(204000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(207000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(210000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(213000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(216000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(219000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(222000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(225000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(228000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(231000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(234000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(237000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(240000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(243000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(246000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(249000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(252000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(255000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(258000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(261000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(264000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(267000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(270000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(273000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(276000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(279000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(282000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(285000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(288000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(291000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(294000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(297000)).toISOString(), 
+    "where": "box" 
+  }
+,{ 
+    "src": "image3.jpg", 
+    "mime": "image/jpeg", 
+    "when":  new Date(new Date().addMilisecons(300000)).toISOString(), 
     "where": "box" 
   }
 ];
